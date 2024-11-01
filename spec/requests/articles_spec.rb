edfc3e648f4c48edc40f198c6e264e29db23552a
Divashen <divashen@ovex.io>
2024-11-01 09:15:14 +0200

    Skip test to drive down test coverage.

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/articles/"
       expect(response).to have_http_status(:success)
     end
